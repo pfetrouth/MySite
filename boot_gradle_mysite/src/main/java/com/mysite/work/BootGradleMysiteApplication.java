ae95d5b88b215a22ba72d97048d0c6d80ee157c8
choinchul <pfetrouth@gmail.com>
2020-02-17 17:20:23 +0900

    [dev-반영] 최초소스

---
+++ boot_gradle_mysite/src/main/java/com/mysite/work/BootGradleMysiteApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGradleMysiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleMysiteApplication.class, args);
+	}
+
+}
---
+++ boot_gradle_mysite/src/test/java/com/mysite/work/BootGradleMysiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGradleMysiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
